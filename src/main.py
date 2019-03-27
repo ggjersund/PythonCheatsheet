801efea88edd8d8fde59541f338fdb61484f357b
Gjert Ingar Gjersund <gjertig@stud.ntnu.no>
2019-03-27 19:32:07 +0100

    Make comment for main.py file

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import classes as classfile
 # Import the listed contents from functions file
 from functions import function, number
 
+# Only run if the file is being run directly
 if __name__ == "__main__":
 
     print("Function only run if file is run directly from this file.")
